7ccf854935c17a0981187ee67f1e14d43c4e26da
akanishev <a.kanishevv@gmail.com>
2024-07-26 19:54:52 +0300

    Update registration.py

--- book_store_testing/registration_login/registration.py
+++ book_store_testing/registration_login/registration.py
@@ -5,8 +5,8 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.get("https://practice.automationtesting.in/")
 
-btn_my_account_menu = driver.find_element_by_xpath("/html/body/div[1]/div[1]/header/div[2]/nav/ul/li[2]/a").click()
 #Регистрация аккаунта.
+btn_my_account_menu = driver.find_element_by_xpath("/html/body/div[1]/div[1]/header/div[2]/nav/ul/li[2]/a").click()
 window_email = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/div/div[2]/form/p[1]/input").send_keys("a.test@gmail.com")
 window_password = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/div/div[2]/form/p[2]/input").send_keys("testpassword251694/")
 btn_register = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/div/div[2]/form/p[3]/input[3]").click()
