d6275e586e554b0aabd030711b03cd22834726fb
akanishev <a.kanishevv@gmail.com>
2024-07-26 19:26:17 +0300

    login.py

--- book_store_testing/registration_login/login.py
+++ book_store_testing/registration_login/login.py
@@ -6,7 +6,7 @@ driver.implicitly_wait(5)
 driver.get("https://practice.automationtesting.in/")
 btn_my_account_menu = driver.find_element_by_xpath("/html/body/div[1]/div[1]/header/div[2]/nav/ul/li[2]/a").click()
 
-#Автаризация аккаунта
+#Авторизация аккаунта
 window_email = driver.find_element_by_css_selector("#username").send_keys("a.test@gmail.com")
 window_password = driver.find_element_by_css_selector("#password").send_keys("testpassword251694/")
 btn_login = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/div/div[1]/form/p[3]/input[3]").click()
