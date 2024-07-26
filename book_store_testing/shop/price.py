dfc10d7c402d2158da12d284a1f0a78453db182e
akanishev <a.kanishevv@gmail.com>
2024-07-26 19:43:37 +0300

    Update price.py

--- book_store_testing/shop/price.py
+++ book_store_testing/shop/price.py
@@ -5,18 +5,20 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get("https://practice.automationtesting.in/")
+
 # Авторизация
 btn_my_account_menu = driver.find_element_by_xpath("/html/body/div[1]/div[1]/header/div[2]/nav/ul/li[2]/a").click()
 window_email = driver.find_element_by_css_selector("#username").send_keys("a.test@gmail.com")
 window_password = driver.find_element_by_css_selector("#password").send_keys("testpassword251694/")
 btn_login = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/div/div[1]/form/p[3]/input[3]").click()
 btn_shop = driver.find_element_by_xpath("/html/body/div[1]/div[1]/header/div[2]/nav/ul/li[1]/a").click()
-#Добавление книги HTML5 WebApp Develpment в корзину.
+
+#Добавить книгу HTML5 WebApp Develpment в корзину.
 btn_add_to_basket = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/ul/li[4]/a[2]").click()
 count_basket = driver.find_element_by_xpath("/html/body/div[1]/div[1]/header/div[2]/nav/ul/li[6]/a/span[1]")
 price_basket = driver.find_element_by_xpath("/html/body/div[1]/div[1]/header/div[2]/nav/ul/li[6]/a/span[2]")
 time.sleep(5)
-
+# Добавить явное ожидание.
 price_check = WebDriverWait(driver,20).until(
      EC.text_to_be_present_in_element((By.XPATH, "/html/body/div[1]/div[1]/header/div[2]/nav/ul/li[6]/a/span[2]"), "180.00")
 )
