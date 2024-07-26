c7fb5ab9d2e535191237942bf7f687ed78206b69
akanishev <a.kanishevv@gmail.com>
2024-07-26 19:53:51 +0300

    Update html5 forms.py

--- book_store_testing/shop/html5 forms.py
+++ book_store_testing/shop/html5 forms.py
@@ -5,13 +5,13 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.get("https://practice.automationtesting.in/")
 
+# Пройти авторизацию.
 btn_my_account_menu = driver.find_element_by_xpath("/html/body/div[1]/div[1]/header/div[2]/nav/ul/li[2]/a").click()
-
 window_email = driver.find_element_by_css_selector("#username").send_keys("a.test@gmail.com")
 window_password = driver.find_element_by_css_selector("#password").send_keys("testpassword251694/")
 btn_login = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/div/div[1]/form/p[3]/input[3]").click()
 time.sleep(4)
-
+# Перейти в магазин, открыть книгу HTML5 Forms, убедиться что текст заголовка соответсвует названию книги.
 btn_shop = driver.find_element_by_xpath("/html/body/div[1]/div[1]/header/div[2]/nav/ul/li[1]/a").click()
 btn_book_html_5_forms = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/ul/li[3]/a[1]/h3").click()
 header_name_book_html_5_forms = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/div[2]/h1")
