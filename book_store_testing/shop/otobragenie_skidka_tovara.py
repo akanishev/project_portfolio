a3eba31579f63635d2ec3ae591507b0fb6916507
akanishev <a.kanishevv@gmail.com>
2024-07-26 19:49:07 +0300

    Update otobragenie_skidka_tovara.py

--- book_store_testing/shop/otobragenie_skidka_tovara.py
+++ book_store_testing/shop/otobragenie_skidka_tovara.py
@@ -7,14 +7,16 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.get("https://practice.automationtesting.in/")
 
+#Авторизоваться
 btn_my_account_menu = driver.find_element_by_xpath("/html/body/div[1]/div[1]/header/div[2]/nav/ul/li[2]/a").click()
-
 window_email = driver.find_element_by_css_selector("#username").send_keys("a.test@gmail.com")
 window_password = driver.find_element_by_css_selector("#password").send_keys("testpassword251694/")
 btn_login = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/div/div[1]/div/div[1]/form/p[3]/input[3]").click()
 time.sleep(4)
+#Перейти в магазин и открыть книгу Android Quick Start Guide. 
 btn_shop = driver.find_element_by_xpath("/html/body/div[1]/div[1]/header/div[2]/nav/ul/li[1]/a").click()
 btn_book_android_quick_start = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/ul/li[1]/a[1]/h3").click()
+#Убедиться что содержимое старой цены = 600.00, а новой = 450.00
 old_price = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/div[2]/div[1]/p/del/span")
 new_price = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/div[2]/div[1]/p/ins/span")
 
